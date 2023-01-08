567ee15eacacf3bb83cd82a7fb4964e723927dca
Yuliya Tonkikh <uliya_i@bk.ru>
2023-01-08 19:11:55 +0700

    add pom.xml

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,6 +1,6 @@
 package ru.netology.stats;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.*;
